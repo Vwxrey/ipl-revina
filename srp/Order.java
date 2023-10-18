6cbe82b00c588a854f0f6a77178edf1f1b0f85fb
REREVINA\DELL <nrrevina@gmail.com>
2023-10-18 11:35:32 +0700

    Ubah sesuai prinsip single responsibility

---
+++ srp/Order.java
@@ -0,0 +1,21 @@
+package srp;
+
+class Order {
+    void calculateTotalSum() {
+    /*...*/
+    }
+    void getItems() {
+    /*...*/
+    }
+    void getItemCount() {
+    /*...*/
+    }
+    void addItem(Item item) {
+    /*...*/
+    }
+    void deleteItem(Item item) {
+    /*...*/
+    }
+    
+    
+}
