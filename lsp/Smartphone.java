bc42f063f1006877bf5192dc5916f3ccbf166e49
REREVINA\DELL <nrrevina@gmail.com>
2023-10-25 03:29:27 +0700

    menambah code awal kasus liskov

---
+++ lsp/Smartphone.java
@@ -0,0 +1,9 @@
+package lsp;
+
+public class Smartphone extends Product{
+    @Override
+    String getName(){
+        return "Samsung S22 Ultra";
+    }
+
+}
---
+++ lsp/Vegetable.java
@@ -0,0 +1,15 @@
+package lsp;
+
+import java.util.Date;
+
+public class Vegetable extends FoodProduct{
+    @Override
+    String getName(){
+        return "Broccoli";
+    }
+    @Override
+    Date getExpiredDate(){
+        return new Date();
+    }
+
+}
